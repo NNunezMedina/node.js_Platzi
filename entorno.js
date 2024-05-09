870f9066bd508c4c0b8d4901a661ab9d2a7ef342
NNunez <nathalynunezmedina@gmail.com>
2024-05-09 16:14:07 -0500

    Definiendo variables de entorno

--- entorno.js
+++ entorno.js
@@ -1,3 +1,6 @@
-let nombre = process.env.NOMBRE;
+//definiendo variables de entorno
+let nombre = process.env.NOMBRE || 'sin nombre';
+let web = process.env.MI_WEB || 'no tengo web';
 
-console.log('Hola' + nombre);
+console.log('Hola ' + nombre);
+console.log('Mi web es ' + web)
