98cb19cf2eda162edc5d5254db3a45880a27d9e8
NNunez <nathalynunezmedina@gmail.com>
2024-05-10 09:51:40 -0500

    third exercise

---
+++ promises.js
@@ -0,0 +1,36 @@
+function hola (nombre) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function() {
+            console.log('Hola ' + nombre);
+            resolve(nombre);
+        },2000);
+    });
+}
+
+function adios (nombre) {
+    return new Promise ( (resolve, reject) => {
+        setTimeout(function () {
+            console.log('Adios ' + nombre);
+            resolve(nombre);
+        },2000)
+    })
+}
+
+function hablar(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            console.log('Bla bla bla bla...');
+            resolve(nombre);
+        },2000);  
+    });
+}
+
+//--
+console.log('Iniciando el proceso...');
+hola('Carlos')
+.then(hablar)
+.then(hablar)
+.then(adios)
+.then((nombre) => {
+    console.log('Terminado el proceso');
+})
